6fbcb4660c1d17870ec647608fa3a1bba399839b
rayan <rayandongol09@gmail.com>
2023-09-18 12:10:13 +0545

    4 websites added

--- kaapa/core/views.py
+++ kaapa/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def registerPage(request):
+    context = {
+
+    }
+    return render(request, '/accounts/register.html', context)
